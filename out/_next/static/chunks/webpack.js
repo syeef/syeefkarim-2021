895bf9701368c170724816166148b68d83aa470a
Syeef Karim <syeef@cloudflare.com>
2021-02-22 23:37:22 +0000

    fix scroll on header by increasing blur - sounds legit

--- out/_next/static/chunks/webpack.js
+++ out/_next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "106cd660c20e165cba20";
+/******/ 	var hotCurrentHash = "147db378387b7000ff84";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
