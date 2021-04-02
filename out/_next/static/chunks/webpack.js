5395b23bfc409a46e353cbf563810e7aeeef9090
Syeef Karim <syeef@cloudflare.com>
2021-04-03 00:23:27 +0100

    footer border on mobile

--- out/_next/static/chunks/webpack.js
+++ out/_next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "eedef44911ceef31cf61";
+/******/ 	var hotCurrentHash = "3868d94370dd2694bf7b";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
